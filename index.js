c825ceabd1021e4981327c66f23ccf4dad3bd05c
Axel <->
2024-04-16 19:40:01 +0300

    3.15-18

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ app.use(cors())
 const Person = require('./models/person')
 
 
-let persons = []
+let n =
+
+
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+    n= persons.length
+  })
+})
 
 app.get('/info', (request, response) => {
-  let n = persons.length
   const res =`
     <p>Phonebook has info for ${n} people</p>
     <p> ${new Date()}</p>
@@ -27,13 +35,7 @@ app.get('/info', (request, response) => {
   response.send(res)
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
-})
-
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person){
       response.json(person)
@@ -41,31 +43,25 @@ app.get('/api/persons/:id', (request, response) => {
       response.status(404).end()
     }
   })
-  .catch(error => {
-    console.log(error)
-    response.status(400).send({ error: 'malformatted id' })
-  })
+  .catch(error => next(error))
   })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
-  
   const body = request.body
   const duplicate = persons.filter(person => person.name === body.name).length
-  console.log(body)
-  console.log(duplicate)
   if (!body.name || !body.number) {
     return response.status(400).json({
       error : 'name or number missing'
     })
   }
-
   if (duplicate != 0) {
     return response.status(400).json({
       error : 'name must be unique'
@@ -83,10 +79,43 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
 const generateId = () => {
   const ID= Math.floor(Math.random()* 10000)
   return ID
 }
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
